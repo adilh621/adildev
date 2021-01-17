f05b99c3781b7bd6b46afa7fee245de6c5b582bb
Adil Hussain <adilh621@gmail.com>
2021-01-16 23:04:07 -0500

    heroku error

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ function App() {
           <Route path='/' exact component={Home} />
           <Route path='/services' component={Services} />
           <Route path='/products' component={Products} />
+          
         </Switch>
       </Router>
     </>
