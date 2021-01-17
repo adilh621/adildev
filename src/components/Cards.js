0d02f764370391a7814190a1230d13703e7ff5b6
Adil Hussain <adilh621@gmail.com>
2021-01-16 22:56:01 -0500

    testing deployment to heroku before finishing up

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -5,15 +5,16 @@ import CardItem from './CardItem';
 function Cards() {
   return (
     <div className='cards'>
-      <h1>Check out these EPIC Destinations!</h1>
+      <h1>Featured Projects!</h1>
       <div className='cards__container'>
         <div className='cards__wrapper'>
           <ul className='cards__items'>
             <CardItem
               src='images/img-9.jpg'
               text='Explore the hidden waterfall deep inside the Amazon Jungle'
-              label='Adventure'
-              path='/services'
+              label='Node.js'
+              path='https://github.com/adilh621'
+              
             />
             <CardItem
               src='images/img-2.jpg'
@@ -21,7 +22,7 @@ function Cards() {
               label='Luxury'
               path='/services'
             />
-                        <CardItem
+            <CardItem
               src='images/img-2.jpg'
               text='Travel through the Islands of Bali in a Private Cruise'
               label='Luxury'
